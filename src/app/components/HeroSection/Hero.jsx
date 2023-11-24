9a3881813ffe38ca25a6a401cecc639b0c95af97
alyhenr <alyssonhra99@gmail.com>
2023-11-24 20:59:45 -0300

    feat: resumes are downloadable now

--- src/app/components/HeroSection/Hero.jsx
+++ src/app/components/HeroSection/Hero.jsx
@@ -7,20 +7,27 @@ import avatar from '../../../assets/avatar.png';
 import HeroTypeAnimation from './HeroTypeAnimation';
 import Link from 'next/link';
 
+const PT_CV = '/cvs/pt_br.pdf';
+const EN_CV = '/cvs/en.pdf';
+
 const Hero = () => {
     const [showCvs, setShowCvs] = useState(false);
 
     const clickOutsideCVS = useCallback(ev => {
         if (showCvs && ev.target.id !== 'cvs') {
-            console.log(ev.target.id);
             setShowCvs(false);
         }
-    })
+    }, [showCvs])
 
     useEffect(() => {
         window.addEventListener("click", clickOutsideCVS);
         return () => { window.removeEventListener("click", clickOutsideCVS); };
     }, [clickOutsideCVS]);
+
+    const downloadCV = url => {
+
+    }
+
     return (
         <section>
             <div className='grid grid-cols-1 lg:grid-cols-12'>
@@ -35,14 +42,13 @@ const Hero = () => {
                         I&apos;m a full stack developer and I&apos;m looking forward to be part of a team and contribute with my skills!
                     </p>
                     <Link
-                        href="/#contact-me"
+                        href="#contact-me"
                         className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 hover:opacity-95 text-white"
                     >
                         Hire Me
                     </Link>
-                    <Link
-                        href="/"
-                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 relative"
+                    <div
+                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 relative cursor-pointer"
                     >
                         <span
                             className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
@@ -52,10 +58,20 @@ const Hero = () => {
                             Download CV
                         </span>
                         <ul className={`${showCvs ? 'inline' : 'hidden'} absolute top-14 p-2 bg-primary-500 rounded-lg [&>li]:m-1 [&>li]:p-2`}>
-                            <li className='hover:bg-primary-400 rounded-xl'>Portuguese</li>
-                            <li className='hover:bg-primary-400 rounded-xl'>English</li>
+                            <li
+                                className='hover:bg-primary-400 rounded-xl'>
+                                <a href={PT_CV} download="alysson-henrique-cv-pt.pdf">
+                                    Portuguese
+                                </a>
+                            </li>
+                            <li
+                                className='hover:bg-primary-400 rounded-xl'>
+                                <a href={EN_CV} download="alysson-henrique-cv-en.pdf">
+                                    English
+                                </a>
+                            </li>
                         </ul>
-                    </Link>
+                    </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-6 lg:mt-0'>
                     <div
