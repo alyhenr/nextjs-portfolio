875460a9e8c374bc9e8a0c34b3b8095c89d2b334
alyhenr <alyssonhra99@gmail.com>
2023-11-24 16:17:32 -0300

    feat: CV button interactive, plus handling UI with clicking outside the button --> next task: download cvs on client machine

--- src/app/components/HeroSection/Hero.jsx
+++ src/app/components/HeroSection/Hero.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Image from 'next/image';
 
 import avatar from '../../../assets/avatar.png';
@@ -8,6 +8,19 @@ import HeroTypeAnimation from './HeroTypeAnimation';
 import Link from 'next/link';
 
 const Hero = () => {
+    const [showCvs, setShowCvs] = useState(false);
+
+    const clickOutsideCVS = useCallback(ev => {
+        if (showCvs && ev.target.id !== 'cvs') {
+            console.log(ev.target.id);
+            setShowCvs(false);
+        }
+    })
+
+    useEffect(() => {
+        window.addEventListener("click", clickOutsideCVS);
+        return () => { window.removeEventListener("click", clickOutsideCVS); };
+    }, [clickOutsideCVS]);
     return (
         <section>
             <div className='grid grid-cols-1 lg:grid-cols-12'>
@@ -29,11 +42,19 @@ const Hero = () => {
                     </Link>
                     <Link
                         href="/"
-                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 relative"
                     >
-                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                        <span
+                            className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
+                            onClick={() => { setShowCvs(prev => !prev); }}
+                            id='cvs'
+                        >
                             Download CV
                         </span>
+                        <ul className={`${showCvs ? 'inline' : 'hidden'} absolute top-14 p-2 bg-primary-500 rounded-lg [&>li]:m-1 [&>li]:p-2`}>
+                            <li className='hover:bg-primary-400 rounded-xl'>Portuguese</li>
+                            <li className='hover:bg-primary-400 rounded-xl'>English</li>
+                        </ul>
                     </Link>
                 </div>
                 <div className='col-span-5 place-self-center mt-6 lg:mt-0'>
